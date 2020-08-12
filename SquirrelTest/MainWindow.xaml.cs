4737f91a1f65d64b35b27e9a03a670b8f23dbf35
Paul Miranda <paulmirve@gmail.com>
2020-08-12 14:22:31 -0700

    first commit

---
+++ SquirrelTest/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Squirrel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SquirrelTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AddVersionNumber();
+            //CheckForUpdates();
+        }
+
+        private async Task CheckForUpdates()
+        {
+            using (var mgr = UpdateManager.GitHubUpdateManager("https://github.com/PaulMirve/SquirrelHelloWorld"))
+            {
+                await mgr.Result.UpdateApp();
+            }
+        }
+
+        private void AddVersionNumber()
+        {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            FileVersionInfo versionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+            GreetingMessage.Text = "This application is currently in its " + versionInfo.FileVersion + " version.";
+            this.Title = $"v.{versionInfo.FileVersion}";
+        }
+    }
+}
